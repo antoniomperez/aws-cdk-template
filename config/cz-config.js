d1ae89772ffe28dba2008aeda8d466403c80d062
Antonio Márquez Pérez <amarquez1980@gmail.com>
2022-02-11 20:11:01 +0100

    chore: #1 checking lint staged

--- config/cz-config.js
+++ config/cz-config.js
@@ -32,7 +32,7 @@ module.exports = {
     { value: 'WIP', name: 'WIP:      Work in progress' },
   ],
 
-  scopes: [{ name: 'AddNewScope' }],
+  scopes: [{ name: 'ChangeMe' }],
 
   allowTicketNumber: true,
   isTicketNumberRequired: false,
